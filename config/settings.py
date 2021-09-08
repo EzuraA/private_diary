9ca0f87aeda3091b2627728281c40ee433691aac
Ann <totoro.daifuku1@gmail.com>
2021-09-08 07:04:36 +0000

    chap9完了

--- config/settings.py
+++ config/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from logging import WARNING
 from pathlib import Path
 
-from django.contrib.messages import constants as messages
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -124,6 +122,8 @@ STATICFILES_DIRS = (
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+
+from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'alert alert-danger',
     messages.WARNING: 'alert alert-warning',
